8a963667350538e5fc48204a20837e9abf74b6e7
Viktor web2025-4 <victorshegelsky@gmail.com>
2025-03-19 21:07:47 +0200

    finished lab4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Command } = require('commander');
-const fs = require('fs');
+const fs = require('fs').promises;
 const http = require('http');
+const { Command } = require('commander');
+const { Builder } = require('xml2js');
 
 const program = new Command();
 
@@ -12,18 +13,57 @@ program
 
 const options = program.opts();
 
-if (!fs.existsSync(options.input)) {
-  console.error('Cannot find input file');
-  process.exit(1);
+async function checkFileExists(filePath) {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running');
-});
-  
-server.listen(options.port, options.host, () => {
-  console.log(`Server running at http://${options.host}:${options.port}/`);
-});
+async function convertJsonToXml(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const jsonData = JSON.parse(data);
 
+    const xmlObj = {
+      data: {
+        exchange: jsonData.map(entry => ({
+          date: entry.exchangedate,
+          rate: entry.rate
+        }))
+      }
+    };
 
+    const builder = new Builder();
+    return builder.buildObject(xmlObj);
+  } catch (error) {
+    console.error('Error reading or converting file:', error);
+    return '<error>Invalid JSON file</error>';
+  }
+}
+
+async function startServer() {
+  if (!(await checkFileExists(options.input))) {
+    console.error('Cannot find input file');
+    process.exit(1);
+  }
+
+  const server = http.createServer(async (req, res) => {
+    try {
+      const xmlData = await convertJsonToXml(options.input);
+      res.writeHead(200, { 'Content-Type': 'application/xml' });
+      res.end(xmlData);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/xml' });
+      res.end('<error>Internal Server Error</error>');
+    }
+  });
+
+  server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}/`);
+  });
+}
+
+startServer();
